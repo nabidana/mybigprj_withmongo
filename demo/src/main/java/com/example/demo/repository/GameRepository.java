d128097502dd63059711c0e28c82ad65c1d0b814
JeWook Lee <123925291+nabidana@users.noreply.github.com>
2023-10-06 16:14:26 +0900

    Delete demo directory

--- demo/src/main/java/com/example/demo/repository/GameRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.demo.domain.GameProduct;
-
-public interface GameRepository extends MongoRepository<GameProduct, String>{
-    
-}
